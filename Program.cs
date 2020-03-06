b339ee58cacb355d60895366055c13a64599ab3a
EastCartel <greatchibyke2016@gmail.com>
2020-03-06 12:42:28 +0100

    initial-commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string input = Console.ReadLine();
+
+               
+
+              
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("There was an error: {0}", ex.Message);
+            }finally
+            {
+                Console.WriteLine("rest of my app ru");
+            }
+             
+
+           
+        }
+    }
+}
---
+++ StringtoInt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCalculator
+{
+    class StringtoInt
+    {
+        public int convert(string input)
+        {
+            try
+            {
+                int convertedNumber;
+                bool isConvertedSuccesfully = int.TryParse(input, out convertedNumber);
+
+                if (!isConvertedSuccesfully)
+                {
+                    throw new Exception("Conversion was not succesful");
+                }
+                return convertedNumber;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
